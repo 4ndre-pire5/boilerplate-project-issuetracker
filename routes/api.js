57e6e66f972cf8bfcde2dbd0cd10c4e86b930b53
Andre Pires <erdnapfaserip@gmail.com>
2025-10-29 12:28:02 -0300

    feat: PUT and DELETE

--- routes/api.js
+++ routes/api.js
@@ -46,27 +46,81 @@ module.exports = function (app) {
 
     .get(async function (req, res) {
       let project = req.params.project;
-      const query = req.query;
+      const query = { ...req.query };
+
+      // Converter 'open' de string para boolean
+      if (query.open !== undefined) {
+        if (query.open === 'true') query.open = true;
+        else if (query.open === 'false') query.open = false;
+      }
 
       try {
         const issues = await Issue.find({ project, ...query }).exec();
         return res.json(issues);
 
-      } catch (err){
+      } catch (err) {
         console.error(err);
         return res.json({ error: "could not fetch issues" });
       }
 
     })
 
-    .put(function (req, res) {
+    .put(async function (req, res) {
       let project = req.params.project;
 
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      } = req.body;
+
+      let { open } = req.body;
+
+      if (!_id) return res.json({ error: "missing _id " });
+
+      const updateFields = {};
+
+      if (issue_title) updateFields.issue_title = issue_title;
+      if (issue_text) updateFields.issue_text = issue_text;
+      if (created_by) updateFields.created_by = created_by;
+      if (assigned_to) updateFields.assigned_to = assigned_to;
+      if (status_text) updateFields.status_text = status_text;
+      if (open !== undefined) {
+        updateFields.open = open === 'true' || open === true ? true : false;
+      } 
+
+      if (Object.keys(updateFields).length === 0) {
+        return res.json({ error: 'no update field(s) sent', _id });
+      }
+
+      updateFields.updated_on = new Date();
+
+      try {
+        const updated = await Issue.findByIdAndUpdate(_id, updateFields, { new: true });
+        if (!updated) return res.json({ error: 'could not update', _id });
+        return res.json({ result: 'successfully updated', _id });
+      } catch (err) {
+        console.error(err);
+        return res.json({ error: 'could not update', _id });
+      }
     })
 
-    .delete(function (req, res) {
-      let project = req.params.project;
+    .delete(async function (req, res) {
+      const { _id } = req.body;
 
+      if (!_id) return res.json({ error: 'missing _id' });
+
+      try {
+        const deleted = await Issue.findByIdAndDelete(_id);
+        if (!deleted) return res.json({ error: 'could not delete', _id });
+        return res.json({ result: 'successfully deleted', _id });
+      } catch (err) {
+        console.error(err);
+        return res.json({ error: 'could not delete', _id });
+      }
     })
 
 };
